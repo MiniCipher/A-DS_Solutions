3907a521b6b99e4a2d3f4b7fff71282cfbcbb372
MiniCipher <108632790+MiniCipher@users.noreply.github.com>
2026-06-02 15:56:49 +0300

    Implement maximum subarray solution

---
+++ 53_Maximum_Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(std::vector<int>& nums) {
+        int current_sum = nums[0];
+        int max_sum = nums[0];
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            current_sum = std::max(nums[i], current_sum + nums[i]);
+            
+            max_sum = std::max(max_sum, current_sum);
+        }
+
+        return max_sum;
+    }
+};
